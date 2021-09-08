cdde36d762a0b193e6950416c945e9e866f061f5
Nadya <snotra.notra@gmail.com>
2021-09-09 00:55:17 +0300

    feat: add adaptative layout

--- js/script.js
+++ js/script.js
@@ -10,3 +10,15 @@ function rang() {
 
 
 rang();
+
+let showMenu = function () {
+    const hamburger = document.querySelector('.hamburger'),
+        menu = document.querySelector('.menu-list');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('menu-list_active');
+        hamburger.classList.toggle('hamburger_active');
+    });
+};
+
+showMenu();
