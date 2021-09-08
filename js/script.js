e0186852d5f37dd83025893bd36420a94f7f53e7
Nadya <snotra.notra@gmail.com>
2021-09-08 11:00:05 +0300

    feat: add highlight lib and format code

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@
 function rang() {
     const line = document.querySelectorAll('.cv__rang-line span'),
         counter = document.querySelectorAll('.cv__rang-counter');
-    console.log(line);
-    console.log(counter);
     counter.forEach((item, i) => {
         line[i].style.width = item.innerHTML;
     });
